5fc727e3edcb68f6f87191e4cb190f43632debe2
Pedro Batista <pedrobatista.420@gmail.com>
2020-10-01 18:50:28 +0100

    search for movie main logic, added img for when poster is not available and stylled a bit :p

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-console.log('Hi there!');
+const fetchData = async (searhTerm) => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: 'a6e9cace',
+            s: searhTerm
+        }
+
+        
+    });
+    
+    if(response.data.Error) {
+        return [];
+    }
+
+    return response.data.Search
+};
+
+const root = document.querySelector('.autocomplete')
+
+root.innerHTML = `
+    <label><b>Search For a Movie</b></label>
+    <input class="input" />
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
+`;
+
+const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
+
+const onInput = async event => {
+    const movies = await fetchData(event.target.value);
+
+    resultsWrapper.innerHTML = '';
+    dropdown.classList.add('is-active');
+    for (let movie of movies) {
+        const item = document.createElement('a');
+
+        const imgSrc = movie.Poster === 'N/A' ? 'img/no_image_available.gif' : movie.Poster
+
+        item.classList.add('dropdown-item');
+        item.innerHTML = `<img src="${imgSrc}" />${movie.Title}`;
+
+        resultsWrapper.appendChild(item);
+    }
+};
+
+input.addEventListener('input', debounce(onInput));
+
+document.addEventListener('click', (event) => {
+    if (!root.contains(event.target)) {
+        dropdown.classList.remove('is-active');
+    }
+});
