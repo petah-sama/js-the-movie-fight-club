3742dc6422eea6570f4e745f74e802a971341e5c
Pedro Batista <pedrobatista.420@gmail.com>
2020-10-05 15:53:37 +0100

    html to show movie details when clicked done

--- js/index.js
+++ js/index.js
@@ -73,5 +73,44 @@ const onMovieSelect = async movie => {
             i: movie.imdbID
         }
     });
-    console.log(response.data);
-}
+    document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+};
+
+const movieTemplate = (movieDetails) => {
+    return `
+        <article class="media">
+            <figure class="media-left">
+                <p class="image">
+                    <img src="${movieDetails.Poster}" />
+                </p>
+            </figure>
+            <div class="media-content">
+                <div class="content">
+                <h1>${movieDetails.Title}</h1>
+                <h4>${movieDetails.Genre}</h4>
+                <p>${movieDetails.Plot}</p>
+                </div>
+            </div>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetails.Awards}</p>
+            <p class="subtitle">Awards</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetails.BoxOffice}</p>
+            <p class="subtitle">Box Office</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetails.Metascore}</p>
+            <p class="subtitle">Metascore</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetails.imdbRating}</p>
+            <p class="subtitle">IMDB Rating</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetails.imdbVotes}</p>
+            <p class="subtitle">IMDB Votes</p>
+        </article>
+    `;
+};
